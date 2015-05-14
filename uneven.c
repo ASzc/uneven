2b90dc511d15f867e195f9fccd9e0cbc4e85625e
Alex Szczuczko <aszczucz@redhat.com>
2015-05-14 13:23:18 +0100

    Cleanup

--- uneven.c
+++ uneven.c
@@ -38,7 +38,6 @@ void source_state_cb(pa_context *c, pa_subscription_event_type_t t, uint32_t idx
 }
 
 void get_volume_cb(pa_context *c, const pa_source_info *i, int eol, void *userdata) {
-    printf("get_volume_cb\n");
     pa_cvolume* volume = userdata;
 
     if (eol < 0) {
@@ -46,20 +45,17 @@ void get_volume_cb(pa_context *c, const pa_source_info *i, int eol, void *userda
     }
 
     if (!eol) {
-        printf("name: %s, #chan: %d, level0: %u, level1: %u\n", i->name, i->volume.channels, i->volume.values[0], i->volume.values[1]);
-
         volume->channels = i->volume.channels;
         int j;
         for (j = 0; j < volume->channels; j++) {
             volume->values[j] = i->volume.values[j];
         }
-        printf("fetched volume ok\n");
     }
 }
 
 volatile sig_atomic_t done = -1;
 
-void cleanup_pa(int signum) {
+void set_terminate(int signum) {
     done = 0;
 }
 
@@ -82,9 +78,9 @@ int force_volume(char* source_name, pa_volume_t desired_volume) {
 
     // Iterate PA mainloop until we're done or the context enters an error state
     pa_volume_t _v[PA_CHANNELS_MAX] = {0U};
-    pa_cvolume current_volume = {0, _v};
+    pa_cvolume current_volume = {0, *_v};
     pa_operation* pending_op = NULL;
-    int do_update = 0;
+    int do_update;
     int i;
     while (done == -1 && pa_state != 2) {
         pa_mainloop_iterate(pa_ml, 1, NULL);
@@ -94,32 +90,26 @@ int force_volume(char* source_name, pa_volume_t desired_volume) {
             // Wait for any pending operation to complete
             if (!pending_op || (pending_op && pa_operation_get_state(pending_op) == PA_OPERATION_DONE)) {
                 if (pending_op) {
-                    printf("<< done\n");
                     pa_operation_unref(pending_op);
                     pending_op = NULL;
                 }
                 switch (state) {
                     case 0:
-                        printf(">> 0\n");
                         // Subscribe to source events
                         pending_op = pa_context_subscribe(pa_ctx, PA_SUBSCRIPTION_MASK_SOURCE, NULL, NULL);
 
                         state = 2;
                         break;
                     case 1:
-                        printf(">> 1\n");
                         // Idle
                         break;
                     case 2:
-                        printf(">> 2\n");
-                        printf("%s\n", source_name);
                         // Source change detected, retrive volume information
                         pending_op = pa_context_get_source_info_by_name(pa_ctx, source_name, get_volume_cb, &current_volume);
 
                         state++;
                         break;
                     case 3:
-                        printf(">> 3\n");
                         // Check channel volumes and reset if required
                         do_update = 0;
                         for (i = 0; i < current_volume.channels; i++) {
@@ -152,14 +142,17 @@ int force_volume(char* source_name, pa_volume_t desired_volume) {
 }
 
 int main(int argc, char* argv[]) {
+    // Two required arguments:
+    //   - source name (ex: alsa_input.pci-0000_00_1b.0.analog-stereo)
+    //   - target percentage volume (ex: 10)
     pa_volume_t volume = (pa_volume_t) (atoi(argv[2]) * (double) PA_VOLUME_NORM / 100);
     if (!PA_VOLUME_IS_VALID(volume)) {
         return 3;
     }
     char* source_name = pa_xstrdup(argv[1]);
 
-    signal(SIGINT, cleanup_pa);
-    signal(SIGTERM, cleanup_pa);
+    signal(SIGINT, set_terminate);
+    signal(SIGTERM, set_terminate);
 
     int retcode = force_volume(source_name, volume);
 
