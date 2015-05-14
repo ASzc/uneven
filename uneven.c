754e27479b57205eddf0d6a90c5f470dad213508
Alex Szczuczko <aszczucz@redhat.com>
2015-05-14 12:50:23 +0100

    Attempting to debug

--- uneven.c
+++ uneven.c
@@ -27,7 +27,7 @@ void pa_state_cb(pa_context *c, void *userdata) {
     }
 }
 
-void force_volume_cb(pa_context *c, pa_subscription_event_type_t t, uint32_t idx, void *userdata) {
+void source_state_cb(pa_context *c, pa_subscription_event_type_t t, uint32_t idx, void *userdata) {
     int* state = userdata;
 
     if ((t & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_SOURCE) {
@@ -38,9 +38,17 @@ void force_volume_cb(pa_context *c, pa_subscription_event_type_t t, uint32_t idx
 }
 
 void get_volume_cb(pa_context *c, const pa_source_info *i, int eol, void *userdata) {
+    printf("get_volume_cb\n");
     pa_cvolume* volume = userdata;
 
+    if (eol < 0) {
+        fprintf(stderr, "Failed to get source information: %s\n", pa_strerror(pa_context_errno(c)));
+    }
+
+    printf("name: %s, #chan: %d, level0: %u, level1: %u\n", i->name, i->volume.channels, i->volume.values[0], i->volume.values[1]);
+
     *volume = i->volume;
+    printf("fetched volume ok\n");
 }
 
 volatile sig_atomic_t done = -1;
@@ -49,7 +57,7 @@ void cleanup_pa(int signum) {
     done = 0;
 }
 
-int force_volume(char* source_name, double desired_volume) {
+int force_volume(char* source_name, pa_volume_t desired_volume) {
     // Create a mainloop
     pa_mainloop* pa_ml = pa_mainloop_new();
     pa_mainloop_api* pa_mlapi = pa_mainloop_get_api(pa_ml);
@@ -64,11 +72,13 @@ int force_volume(char* source_name, double desired_volume) {
 
     // Set up event subscription
     int state = 0;
-    pa_context_set_subscribe_callback(pa_ctx, force_volume_cb, &state);
+    pa_context_set_subscribe_callback(pa_ctx, source_state_cb, &state);
 
     // Iterate PA mainloop until we're done or the context enters an error state
     pa_cvolume* current_volume = NULL;
     pa_operation* pending_op = NULL;
+    int do_update = 0;
+    int i;
     while (done == -1 && pa_state != 2) {
         pa_mainloop_iterate(pa_ml, 1, NULL);
 
@@ -77,29 +87,35 @@ int force_volume(char* source_name, double desired_volume) {
             // Wait for any pending operation to complete
             if (!pending_op || (pending_op && pa_operation_get_state(pending_op) == PA_OPERATION_DONE)) {
                 if (pending_op) {
+                    printf("<< done\n");
                     pa_operation_unref(pending_op);
                     pending_op = NULL;
                 }
                 switch (state) {
                     case 0:
+                        printf(">> 0\n");
                         // Subscribe to source events
                         pending_op = pa_context_subscribe(pa_ctx, PA_SUBSCRIPTION_MASK_SOURCE, NULL, NULL);
 
-                        state++;
+                        state = 2;
                         break;
                     case 1:
+                        printf(">> 1\n");
                         // Idle
                         break;
                     case 2:
+                        printf(">> 2\n");
+                        printf("%s\n", source_name);
                         // Source change detected, retrive volume information
                         pending_op = pa_context_get_source_info_by_name(pa_ctx, source_name, get_volume_cb, current_volume);
 
                         state++;
                         break;
                     case 3:
+                        printf(">> 3\n");
                         // Check channel volumes and reset if required
-                        int do_update = 0;
-                        for (int i = 0; i < current_volume->channels; i++) {
+                        do_update = 0;
+                        for (i = 0; i < current_volume->channels; i++) {
                             if (current_volume->values[i] != desired_volume) {
                                 do_update = 1;
                                 current_volume->values[i] = desired_volume;
@@ -129,11 +145,11 @@ int force_volume(char* source_name, double desired_volume) {
 }
 
 int main(int argc, char* argv[]) {
-    double volume = atoi(argv[1]) * (double) PA_VOLUME_NORM / 100;
-    if (!PA_VOLUME_IS_VALID((pa_volume_t) volume)) {
+    pa_volume_t volume = (pa_volume_t) (atoi(argv[2]) * (double) PA_VOLUME_NORM / 100);
+    if (!PA_VOLUME_IS_VALID(volume)) {
         return 3;
     }
-    char* source_name = pa_xstrdup(argv[0]);
+    char* source_name = pa_xstrdup(argv[1]);
 
     signal(SIGINT, cleanup_pa);
     signal(SIGTERM, cleanup_pa);
